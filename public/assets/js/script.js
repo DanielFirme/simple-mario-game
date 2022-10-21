6818aa308072fb570cb2e22b55adefdd63c641fc
Daniel Firme <danielfirme.github@gmail.com>
2022-10-20 22:49:56 -0300

    completion of basic game operation

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,25 @@
 "use strict";
+const $ = document.querySelector.bind(document);
+const mario = $('.mario');
+const pipe = $('.pipe');
+window.addEventListener('keydown', jump);
+function jump() {
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+function main() {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    if (pipePosition <= 90 && pipePosition > 0 && marioPosition <= 65) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+        mario.style.animation = 'none';
+        mario.src = 'assets/images/game-over.png';
+        mario.style.height = '66px';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.style.left = '50px';
+    }
+}
+const loop = setInterval(main, 10);
